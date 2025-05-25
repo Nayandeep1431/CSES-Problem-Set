432cc3d72f4f41681be02c9d02892948a3e99080
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2025-05-25 15:36:12 +0530

    Create Permutations.cpp
    Permutations (CSES)
    
    Problem Link : https://cses.fi/problemset/task/1070
    
    Difficulty Level : Easy

---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int n) {
+    if (n == 2 || n == 3) {
+        cout << "NO SOLUTION";
+        return;
+    }
+
+    for (int i = 2; i <= n; i += 2)
+        cout << i << " ";
+
+    for (int i = 1; i <= n; i += 2)
+        cout << i << " ";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    solve(n);
+}
