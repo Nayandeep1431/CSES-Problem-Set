fb79dd9bfe19a84703741cbe1c51ff70c95927ef
Nayandeep1431 <154627577+Nayandeep1431@users.noreply.github.com>
2025-05-30 22:21:11 +0530

    Create Apartments.cpp
    Apartments 
    (CSES)
    
    Problem Link : https://cses.fi/problemset/task/1084
    
    Difficulty Level : Medium

---
+++ Sorting and Searching/Apartments.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    vector<int> applicants(n);
+    vector<int> apartments(m);
+
+    for (int i = 0; i < n; ++i) cin >> applicants[i];
+    for (int i = 0; i < m; ++i) cin >> apartments[i];
+
+    sort(applicants.begin(), applicants.end());
+    sort(apartments.begin(), apartments.end());
+
+    int i = 0, j = 0, count = 0;
+    while (i < n && j < m) {
+        if (abs(applicants[i] - apartments[j]) <= k) {
+            count++;
+            i++;
+            j++;
+        }
+        else if (apartments[j] < applicants[i] - k) {
+            j++;
+        }
+        else {
+            i++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
